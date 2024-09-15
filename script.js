59cafec20e0fac65be005c86771166dcb646c57d
franck403 <79372051+franck403@users.noreply.github.com>
2024-09-15 08:59:38 -0400

    update

---
+++ script.js
@@ -0,0 +1,56 @@
+function request(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false); // Set the third parameter to false for synchronous request
+  
+    try {
+      xhr.send();
+      if (xhr.status === 200) {
+        const text = xhr.responseText;
+        return text;
+      } else {
+        console.error("Error:", xhr.status, xhr.statusText);
+      }
+    } catch (error) {
+      console.error("Request failed:", error);
+    }
+  }
+  var lastid = "";
+  function loadInfo(id) {
+    console.log(lastid,id)
+    if (lastid == id) {
+      return;
+    } else {
+      var lastid = id;
+    }
+    var information = request(
+      "https://world.openfoodfacts.org/api/v*/product/" + id
+    );
+    var information = JSON.parse(information);
+    document.getElementById(
+      "result"
+    ).innerHTML = 'Ingredients : ' + information.product.ingredients_text.replaceAll("*", "");
+  console.log(information) 
+    document.getElementById("quantity").innerHTML = ' Quantité : ' + information.product.quantity;
+    document.getElementById("trace").innerHTML = 'Ingredient avec un risque d' + "'" + 'allergie : ' + information.product.allergens.replaceAll('en:','');
+    document.getElementById("scoreIMG").src =
+      "https://static.openfoodfacts.org/images/attributes/dist/nutriscore-" +
+      information.product.nutriscore["2023"].grade +
+      "-new-en.svg";
+  }
+  function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    loadInfo(decodedText)
+  }
+  
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+  }
+  
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: { width: 250, height: 250 } },
+    /* verbose= */ false
+  );
+  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
